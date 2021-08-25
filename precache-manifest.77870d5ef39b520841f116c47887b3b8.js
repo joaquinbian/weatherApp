8b0c6ae2f70d96426ca5fc26f4f2326d77e1b0c4
joaquinbian <joacobian21@gmail.com>
2021-08-25 12:00:44 -0300

    Updates

---
+++ precache-manifest.77870d5ef39b520841f116c47887b3b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6571351a21c5bfd5b969ad73e62cd3",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "8a3ae129afab67bc34ad",
+    "url": "/weatherApp/static/css/main.15e0def0.chunk.css"
+  },
+  {
+    "revision": "2820400b0c2063702797",
+    "url": "/weatherApp/static/js/2.f5e61927.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weatherApp/static/js/2.f5e61927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3ae129afab67bc34ad",
+    "url": "/weatherApp/static/js/main.352212e0.chunk.js"
+  },
+  {
+    "revision": "b855f00ea594d19116bc",
+    "url": "/weatherApp/static/js/runtime-main.fe161b37.js"
+  }
+]);
